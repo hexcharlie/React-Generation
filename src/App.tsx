cfda5140ae753b807dd66d9edf85113648e89112
hexcharlie <luishenriquemf23@hotmail.com>
2023-10-04 14:40:42 -0300

    login e cadastro

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,34 @@
 import React from 'react';
 import './App.css';
-import Home from './paginas/home/Home';
+
+import Navbar from './components/navBar/NavBar';
+import Footer from './components/footer/Footer';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './paginas/Login/Login';
+import Cadastro from './paginas/cadastro/Cadastro';
+import Home from './paginas/home/Home';
+import { AuthProvider } from './contexts/AuthContext';
+import ListaTemas from './components/Temas/listaTemas/ListaTemas';
+
+
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/Login" element={<Login />} />
-          <Route path="/home" element={<Home />} />
-        </Routes>
-      </BrowserRouter>
+    <AuthProvider>
+        <BrowserRouter>
+          <Navbar />
+          <div className='min-h-[80vh]'>
+            <Routes>
+              <Route path="/" element={<Login />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/cadastro" element={<Cadastro />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/temas" element={<ListaTemas />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+        </AuthProvider>
     </>
   );
 }
